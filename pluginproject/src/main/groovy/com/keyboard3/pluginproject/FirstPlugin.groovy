0160beb34a54a9cb8fcefa2c8bdcbb71771d8314
keyboard3 <keyboard3@icloud.com>
2017-12-31 16:12:01 +0800

    独立的pluginproject插件 build.gradle内部使用的插件

--- pluginproject/src/main/groovy/com/keyboard3/pluginproject/FirstPlugin.groovy
+++ pluginproject/src/main/groovy/com/keyboard3/pluginproject/FirstPlugin.groovy
@@ -1,13 +1,13 @@
-package  com.keyboard3.buildsrc
+package  com.keyboard3.pluginproject
 
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 
-public class SecondPlugin implements Plugin<Project> {
+public class FirstPlugin implements Plugin<Project> {
 
     void apply(Project project) {
         System.out.println("========================");
-        System.out.println("这是buildsrc插件!");
+        System.out.println("这是pluginproject插件!");
         System.out.println("========================");
     }
 }
